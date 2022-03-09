a87894fb917c524d69357b5ff4d46fde6e38ab51
Alexandre Clavel <Alexandre.Clavel.2000@gmail.com>
2022-03-09 15:13:33 -0600

    Adding work for floodit

--- lab1/flood-it/floodit.py
+++ lab1/flood-it/floodit.py
@@ -6,25 +6,55 @@ class Node:
     def add_neighbor(self,node) -> None :
         self.neighbors.append(node)
 
-    def get_best(self) -> int:
-        diff_nodes = self.get_diff_neighbors(set())
+    def play_best(self) -> int:
+        diff_nodes = set()
+        diff_nodes = self.get_diff_neighbors(diff_nodes)
         values = [0]*7
         for node in diff_nodes:
             values[node.value] += 1
-        
-        return values.index(values.max)
+        values[self.value] = 0
 
-    def get_neighbors(self, visited: set) -> None:
+        old_value = self.value
+        new_value = values.index(max(values))
+        for node in diff_nodes:
+            if node.value == old_value:
+                node.value = new_value
+        return new_value
+
+    def score(self, target:int, visited, curr_score):
+        visited.add(self)
         for node in self.neighbors:
-            visited.add(self)
+            if node not in visited:
+                if node.value == self.value or node.value == target:
+                    visited, curr_score = node.score(target, visited, curr_score)
+                if node.value == target:
+                    curr_score += 1
+        return visited, curr_score
+
+    def get_same_neighbors(self, visited) -> set:
+        visited.add(self)
+        for node in self.neighbors:
+            if node not in visited and node.value == self.value:
+                visited = node.get_same_neighbors(visited)
+        return visited
+
+    def get_diff_neighbors(self, visited: set) -> set:
+        visited.add(self)
+        for node in self.neighbors:
             if node not in visited:
                 if node.value == self.value:
-                    node.get_diff_neighbors(visited)
+                    visited = node.get_diff_neighbors(visited)
                 else:
-                    visited.add(node)
+                    visited = node.get_same_neighbors(visited)
+        return visited
     
     def __repr__(self) -> str:
         return str(self.value)
+
+def arrprint(arr):
+    for row in arr:
+        print(row)
+    print()
 
 if __name__ == '__main__':
     num_boards = int(input())
@@ -33,4 +63,24 @@ if __name__ == '__main__':
         node_arr = []
         for i in range(width):
             node_arr.append(list(map(Node, map(int, input()))))
-        print(node_arr)
+        
+        #Populate Nodes
+        for i in range(width):
+            for j in range(width):
+                if i - 1 >= 0:
+                    node_arr[i][j].add_neighbor(node_arr[i-1][j])
+                if j - 1 >= 0:
+                    node_arr[i][j].add_neighbor(node_arr[i][j-1])
+                if i + 1 < width:
+                    node_arr[i][j].add_neighbor(node_arr[i+1][j])
+                if j + 1 < width:
+                    node_arr[i][j].add_neighbor(node_arr[i][j+1])
+
+        moves = [0]*6
+        next_move = node_arr[0][0].play_best()
+        while next_move != 0:
+            moves[next_move - 1] += 1
+            next_move = node_arr[0][0].play_best()
+
+        print(sum(moves))
+        print(*moves)
