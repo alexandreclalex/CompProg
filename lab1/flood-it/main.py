06a03dbde16e5f95159b91c82986588bb3574b27
NathanDuPont <nathan.dupont01@gmail.com>
2022-03-14 04:16:46 -0500

    Adding fixed flood-it array solution and report

--- lab1/flood-it/main.py
+++ lab1/flood-it/main.py
@@ -1,96 +1,99 @@
 
-def read_file(path):
+def read_input():
     game_grids = {}
-    with open(path, 'r') as f:
-        num_test_cases = int(f.readline())
+    num_test_cases = int(input())
 
-        for i in range(num_test_cases):
-            grid_size = int(f.readline())
+    for i in range(num_test_cases):
+        grid_size = int(input())
 
-            l1 = []
-            for _ in range(grid_size):
-                l2 = []
-                line = f.readline()
+        l1 = []
+        for _ in range(grid_size):
+            l2 = []
+            line = input()
 
-                for j in range(grid_size):
-                    l2.append(int(line[j]))
+            for j in range(grid_size):
+                l2.append(int(line[j]))
 
-                l1.append(l2)
+            l1.append(l2)
 
-            game_grids[i] = l1
+        game_grids[i] = l1
 
     return game_grids
 
-def search_neighbors(board, color, x=0, y=0, checked=[], occurrences=[], current_arr=[]):
-    #print(f"Searching ({x}, {y})")
+def generate_path(board, color, x=0, y=0, checked=set(), path=set(), neighbors=set()):
     size = len(board[0])
 
     if (x >= 0 and x < size) and (y >= 0 and y < size) and (x,y) not in checked:
-        checked.append((x,y))
+        checked.add((x,y))
 
         current_color = board[y][x]
-       
+
         if current_color == color:
-            current_arr.append((x, y))
+            path.add((x,y))
 
-            (occurrences, current_arr) = search_neighbors(board, color, x, y-1, checked, occurrences, current_arr)
-
-            #(occurrences, current_arr) = search_neighbors(board, color, x+1, y-1, checked, occurrences, current_arr)
-            
-            (occurrences, current_arr) = search_neighbors(board, color, x+1, y, checked, occurrences, current_arr)
-            
-            #(occurrences, current_arr) = search_neighbors(board, color, x+1, y+1, checked, occurrences, current_arr)
-            
-            (occurrences, current_arr) = search_neighbors(board, color, x, y+1, checked, occurrences, current_arr)
-            
-            #(occurrences, current_arr) = search_neighbors(board, color, x-1, y+1, checked, occurrences, current_arr)
-            
-            (occurrences, current_arr) = search_neighbors(board, color, x-1, y, checked, occurrences, current_arr)
-            
-            #(occurrences, current_arr) = search_neighbors(board, color, x-1, y-1, checked, occurrences, current_arr)
+            (checked, path, neighbors) = generate_path(board, color, x, y-1, checked, path, neighbors)
+            (checked, path, neighbors) = generate_path(board, color, x+1, y, checked, path, neighbors)
+            (checked, path, neighbors) = generate_path(board, color, x, y+1, checked, path, neighbors)
+            (checked, path, neighbors) = generate_path(board, color, x-1, y, checked, path, neighbors)
         else:
-            occurrences.append(current_color)
+            neighbors.add((x,y))
 
-    return occurrences, current_arr
+    return checked, path, neighbors
 
 
 def play_game(board):
     steps = {}
 
-    for i in range(5):
-        occurrences, current_arr = search_neighbors(board, board[0][0], 0, 0, [], [], [])
+    while True:
+        (checked, path, neighbors) = generate_path(board, board[0][0], 0, 0, set(), set(), set())
 
-        if len(current_arr) == len(board[0]) ** 2:
+        # If all squares are colored the same, end game
+        if len(path) == len(board[0]) ** 2:
             return steps
 
-        occurrences_dict = {}
-        for element in occurrences:
-            occurrences_dict[element] = occurrences_dict.get(element, 0) + 1
+        neighboring_colors = {}
 
-        max_val = (1, 0)
-        for key in occurrences_dict.keys():
-            val = occurrences_dict[key]
-            if val > max_val[1]:
-                max_val = (key, val) 
-            elif val == max_val[1] and key < max_val[0]:
-                max_val = (key, val)
+        visited = path.copy()
+        for neighbor in neighbors:
+            x = neighbor[0]
+            y = neighbor[1]
+            color = board[y][x]
 
-        for (x, y) in current_arr:
-            board[y][x] = max_val[0]
+            (new_checked, new_path, _) = generate_path(board, color, x, y, visited.copy(), set(), set())
 
-        steps[max_val[0]] = steps.get(max_val[0], 0) + 1
+            visited = visited.union(new_path)
 
+            neighboring_colors[color] = neighboring_colors.get(color, 0) + len(new_path)
+
+        next_color = None
+        max_val = max(neighboring_colors.values())
+
+        for color in neighboring_colors.keys():
+            if neighboring_colors[color] == max_val and (next_color is None or color < next_color):
+                next_color = color
+
+        for (x, y) in path:
+            board[y][x] = next_color
+
+        steps[next_color] = steps.get(next_color, 0) + 1
 
 
 def main():
-    game_grids = read_file("lab1/flood-it/sample-input.txt")
+    game_grids = read_input()
     
     for game in game_grids.keys():
         game_board = game_grids[game]
 
         steps = play_game(game_board)
 
-        print(steps)
+        steps_total = 0
+        steps_str = ""
+
+        for i in range(1, 7):
+            steps_total += steps.get(i, 0)
+            steps_str += f"{steps.get(i, 0)} "
+
+        print(f"{steps_total}\n{steps_str}")
 
 
 if __name__ == "__main__":
