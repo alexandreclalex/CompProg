8bf9a95b3fe8cfe5ccd69b083d0a8435b165b0eb
Alexandre Clavel <Alexandre.Clavel.2000@gmail.com>
2022-03-21 17:04:42 -0500

    Adding comments for Hermits

--- lab2/Hermits/hermits.py
+++ lab2/Hermits/hermits.py
@@ -1,3 +1,4 @@
+from multiprocessing.reduction import duplicate
 import sys
 
 #Read in the Input
@@ -5,12 +6,17 @@ numstreets = int(sys.stdin.readline())
 populations = list(map(int, sys.stdin.readline().split(' ')))
 num_indices = int(sys.stdin.readline())
 indices = []
+#Parse indices into lists
 for i in range(num_indices):
     indices.append(list(map(int, sys.stdin.readline().split(' '))))
 
+#Duplicate population array, we will add the adjoining populations to this array
 adj_pop = populations.copy()
+
+#Iterate through indices and add populations
 for a, b in indices:
     adj_pop[a - 1] += populations[b - 1]
     adj_pop[b - 1] += populations[a - 1]
 
+#Find and print the street with the least population
 print(adj_pop.index(min(adj_pop)) + 1)
