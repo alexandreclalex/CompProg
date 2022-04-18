1d804fcc84af6a12437fcaf048637b9b0db88337
Alexandre Clavel <Alexandre.Clavel.2000@gmail.com>
2022-04-18 01:08:54 -0500

    Adding code for elegantshowroom

--- lab6/elegantshowroom/main.py
+++ lab6/elegantshowroom/main.py
@@ -1,3 +1,4 @@
+from collections import deque
 import sys
 
 rows, cols = list(map(int, sys.stdin.readline()[:-1].split(' ')))
@@ -7,34 +8,24 @@ for row in range(rows):
 
 x, y = list(map(int, sys.stdin.readline()[:-1].split(' ')))
 x, y = x-1, y-1
-b[x][y] = 0
+b[x][y] = 1
 
 moves = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-
-
-def search(x, y, board):
-    if board[x][y] == '#':
-        return board
+queue = deque()
+queue.append((x, y))
+while len(queue) > 0:
+    x, y = queue.popleft()
     for dx, dy in moves:
-        print(dx, dy)
-        if x + dx > 0 and x + dx < rows:
-            if board[x+dx][y] == 'c':
-                board[x+dx][y] = board[x][y] + 1
-            elif board[x+dx][y] == 'D':
-                board[x+dx][y] = board[x][y]
-            elif board[x+dx][y] > board[x][y] + 1:
-                board[x+dx][y] = board[x][y] + 1
-            board = search(x+dx, y, board)
-        if y + dy > 0 and y + dy < cols:
-            if board[x][y+dy] == 'c':
-                board[x][y+dy] = board[x][y] + 1
-            elif board[x][y+dy] == 'D':
-                board[x][y+dy] = board[x][y]
-            elif board[x][y+dy] > board[x][y] + 1:
-                board[x][y+dy] = board[x][y] + 1
-            board = search(x, y+dy, board)
-    return board
-
-board = search(x, y, b)
-print(board)
+        if x + dx >= 0 and x + dx < len(b) and y + dy >= 0 and y + dy < len(b[0]):
+            if b[x+dx][y+dy] == 'c':
+                b[x+dx][y+dy] = b[x][y] + 1
+                queue.append((x+dx, y+dy))
+            elif b[x+dx][y+dy] == 'D':
+                if x+dx == 0 or x+dx == len(b)-1 or y+dy == 0 or y+dy == len(b[0])-1:
+                    print(b[x][y])
+                    queue.clear()
+                else:
+                    b[x+dx][y+dy] = b[x][y]
+                    queue.appendleft((x+dx, y+dy))
+        
