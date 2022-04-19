e1bd6d77d58bc9a66cf5c2fe807db830d0b3638a
Alexandre Clavel <Alexandre.Clavel.2000@gmail.com>
2022-04-19 14:14:08 -0500

    Fixing erdos numbers

--- lab6/erdosnumbers/main.py
+++ lab6/erdosnumbers/main.py
@@ -11,21 +11,17 @@ authors = dict()
 inline = sys.stdin.readline()[:-1]
 while inline != '':
     tokens = inline.split(' ')
-    
-    # Add target
     targets.append(tokens[0])
-    tokens = set(tokens)
+    if tokens[0] not in authors:
+        authors[tokens[0]] = set(tokens[1:])
+    else:
+        authors[tokens[0]].update(tokens[1:])
     
-    # Add coauthors to authors dict
-    for elem in tokens:
-        if elem not in authors:
-            authors[elem] = tokens.copy()
-        else:
-            authors[elem].update(tokens)
-        
-        # Purge self from co-authors
-        authors[elem].remove(elem)
-    
+    for i in range(1, len(tokens)):
+        if tokens[i] not in authors:
+            authors[tokens[i]] = set()
+        authors[tokens[i]].add(tokens[0])
+
     inline = sys.stdin.readline()[:-1]
 
 # Initialize container for Erdos nums
@@ -33,24 +29,24 @@ erdos_nums = dict()
 erdos_nums['PAUL_ERDOS'] = 0
 
 # Initialize Queue to explore
-queue =deque()
+queue = deque()
 queue.append('PAUL_ERDOS')
 
 # Iterate through queue
 while len(queue) > 0:
     next_author = queue.popleft()
     next_score = erdos_nums[next_author]
-    
+
     # Iterate through co-authors
-    for co_authors in authors[next_author]:
+    for co_author in authors[next_author]:
         # Check if we can assign a better score to the co-author
-        if co_authors not in erdos_nums or erdos_nums[co_authors] > next_score + 1:
-            erdos_nums[co_authors] = next_score + 1
-            
+        if co_author not in erdos_nums or erdos_nums[co_author] > next_score + 1:
+            erdos_nums[co_author] = next_score + 1
+
             # Add co-author to the queue
-            queue.append(co_authors)
-            
-#Iterate through targets and print their scores
+            queue.append(co_author)
+
+# Iterate through targets and print their scores
 for target in targets:
     sys.stdout.write(target + ' ')
     if target in erdos_nums:
