ffe1782f1efa5fb7f9af42a909e1d53cdfa17567
David <yangda@msoe.edu>
2022-05-03 18:46:30 -0500

    Attempt two at airport coffee

--- lab8/airportcoffee/main.py
+++ lab8/airportcoffee/main.py
@@ -8,41 +8,51 @@ n = int(sys.stdin.readline()[:-1])
 carts = list(map(int, sys.stdin.readline().split()))
 
 # Look for the nearest cart that is ahead of the person.
-def findNearestCart(current_distance):
-    cart = -1
-    for x in carts:
-        if current_distance <= x:
-            cart = x
-            break
-    return cart
-
-    #Attempt to find the nearest cart
-    # value = (carts[min(range(len(carts)), key=lambda i: abs(carts[i] - current_distance))])
-    # index = carts.index(value)
-    # if current_distance > value and index != len(carts)-1:
-    #     return carts[index+1]
-    # elif current_distance < value:
-    #     return value
-    # else:
-    #     return -1
+# def findNearestCart(current_distance):
+#     cart = -1
+#     for x in carts:
+#         if current_distance <= x:
+#             cart = x
+#             break
+#     return cart
+#     #Attempt to find the nearest cart
+#     value = (carts[min(range(len(carts)), key=lambda i: abs(carts[i] - current_distance))])
+#     index = carts.index(value)
+#     if current_distance > value and index != len(carts)-1:
+#         return carts[index+1]
+#     elif current_distance < value:
+#         return value
+#     else:
+#         return -1
 
 if __name__ == '__main__':
-    current_distance = 0
-    coffee = False
-    inc = 0
+    # FIRST ATTEMPT
+    # current_distance = 0
+    # purchase = []
+    # coffee = False
+    # inc = 0
+    # cart = findNearestCart(current_distance)
+    # while current_distance < l:
+    #     # Should person drink from coffee stand?
+    #     if (current_distance + inc) >= cart:
+    #         inc = b
+    #         current_distance += (a * wait + b * drink)
+    #         if cart >= 0:
+    #             purchase.append(carts.index(cart))
+    #         cart = findNearestCart(current_distance)
+    #     else:
+    #         # inc = a
+    #         current_distance += (cart - current_distance)
+
+    # SECOND ATTEMPT
+    # This may be much faster when I find the correct solution
+
     purchase = []
-    cart = findNearestCart(current_distance)
-    while current_distance < l:
-        # Should person drink from coffee stand?
-        if (current_distance + inc) >= cart:
-            inc = b
-            current_distance += (a * wait + b * drink)
-            if cart >= 0:
-                purchase.append(carts.index(cart))
-            cart = findNearestCart(current_distance)
-        else:
-            inc = a
-            current_distance += a
+    current_distance = 0
+    for x in carts:
+        if current_distance < x:
+            current_distance += x - current_distance + (a * wait + b * drink)
+            purchase.append(carts.index(x))
 
     print(len(purchase))
     print(' '.join(map(str, purchase)))
