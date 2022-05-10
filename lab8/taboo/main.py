bc11bb066efaa9124f7709e26f35df0f6b4119d3
NathanDuPont <nathan.dupont01@gmail.com>
2022-05-10 14:03:02 -0500

    final lab 8 report

--- lab8/taboo/main.py
+++ lab8/taboo/main.py
@@ -1,11 +1,14 @@
 import sys
 
+# Save possible values
 possible_vals = set()
 remaining_vals = set()
 min_len = 1000000000
 min_len = 0
+max_val = 0
 
 
+# Create node for tree
 class Node:
     def __init__(self, val=None):
         self.val = val
@@ -19,43 +22,36 @@ class Node:
         return next((val for val in self.children), None)
 
 
+# Create Trie data structure
 class Trie:
     def __init__(self):
         self.root = Node()
 
 
-if __name__ == '__main__':       
+if __name__ == '__main__':   
+    # Read in the number of clues    
     num_lines = int(sys.stdin.readline()[:-1])
 
     clues = set()
 
+    # For each line, read in the clues
     for i in range(num_lines):
         clue_binary_str = sys.stdin.readline().strip('\n')
         clue_val = int(clue_binary_str, 2)
-        # max_val = int(''.join(["1"]*len(clue_binary_str)), 2)
+        current_max = int(''.join(["1"]*len(clue_binary_str)), 2)
 
+        # Keep track of the min and max length
         min_len = min(min_len, len(clue_binary_str))
         max_len = max(max_len, len(clue_binary_str))
 
     for i in range(min_len, max_len):
+        if max_val > current_max:
+            for j in range(max_val - current_max):
+                possible_vals.add(j + current_max)
 
+                if j + current_max != clue_val:
+                    remaining_vals.add(j + current_max)
+        else:
+            remaining_vals.discard(clue_val)
 
-        # print(clue_val)
-        # print(clue_val >> 1)
-
-        print(len(clue_val))
-        print(len(clue_val >> 1))
-
-        for i in range(min_len, max_len):
-            
-
-
-
-        # if max_val > current_max:
-        #     for j in range(max_val - current_max):
-        #         possible_vals.add(j + current_max)
-
-        #         if j + current_max != clue_val:
-        #             remaining_vals.add(j + current_max)
-        # else:
-        #     remaining_vals.discard(clue_val)
+    print(max(possible_vals))
